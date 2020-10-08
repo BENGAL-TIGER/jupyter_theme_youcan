4330cf613a2e5d26d7ba373f7c6fd2d73d366ee5
mdAshford <marcus.ashford@ua.edu>
2020-10-08 17:44:46 -0500

    prep for npm

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ import { IThemeManager } from  '@jupyterlab/apputils';
  * A plugin for myorg/jupyter_theme_youcan
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: '@myorg/jupyter_theme_youcan:plugin',
+  id: '@marcucius/jupyter_theme_youcan:plugin',
   requires: [IThemeManager],
   activate: function(app: JupyterFrontEnd, manager: IThemeManager) {
-    const style = '@myorg/jupyter_theme_youcan/index.css';
+    const style = '@marcucius/jupyter_theme_youcan/index.css';
 
     manager.register({
       name: 'jupyter_theme_youcan',
